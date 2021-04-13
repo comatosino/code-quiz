0671e1f18040e5f6760bcd8d5cab24ea9ec81edf
Robert Adams <adamsiii.robert@gmail.com>
2021-04-13 12:29:26 -0700

    added event listeners for save/scores/back btns

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // create an array of question objects
-var questionArray = [
+const questionArray = [
     {
     question: "question1", 
     answer1: "q1a1", 
@@ -37,12 +37,18 @@ var questionArray = [
     }
 ];
 
-// let's make some pointers
-var playButton = document.getElementById("play-btn");
-var gameHeading = document.getElementById("game-heading");
-var timerBox = document.getElementById("timer-box");
-var gameBox = document.getElementById("game-box");
-var feedbackBox = document.getElementById("feedback-box");
+// buttons
+const playBtn = document.getElementById("play-btn");      // starts a new game
+const saveBtn = document.getElementById("save-btn");      // saves a high score
+const clearBtn = document.getElementById("clear-btn");    // clears high scores
+const scoresBtn = document.getElementById("scores-btn");  // displays high scores
+const backBtn = document.getElementById("back-btn");      // go from high scores to start page
+
+const timerBox = document.getElementById("timer-box");           // displays time remaining
+const gameBox = document.getElementById("game-box");             // holds question/answers
+const scoreTable = document.getElementById("high-scores-table"); // holds high scores
+const feedbackBox = document.getElementById("feedback-box");     // displays feedback
+const gameHeading = document.getElementById("game-heading");
 
 // game variables
 var highScores = getLocalHighScores(); // retrieve high scores from local memory
@@ -51,29 +57,95 @@ var gameTimer;
 var timeLeft = startTime;
 var index;
 
-var saveButton = document.getElementById("save-btn");
-var initials = document.getElementById("initials-input");
+// plays the game
+playBtn.addEventListener("click", function (event) {
+    timeLeft = startTime;
+    index = 0;
+
+    // feedbackBox.style.display = "block";
+    // feedbackBox.textContent = "";
+    gameHeading.textContent = "Good Luck!";
+
+    // start the timer
+    gameTimer = setInterval(function() {
+        timerBox.textContent = "Time Left: " + timeLeft;
+        timerBox.style.visibility = "visible";
+
+        // if no time is left, player loses
+        if (timeLeft <= 0) {
+            timerBox.style.visibility = "hidden";
+            defeat();
+        }
+        timeLeft--;
+
+        //updates every second
+    }, 1000);
+
+    setTimeout(function() {
+        gameHeading.textContent = `Click "Play" to start!`;
+        writeQuestion(index);
+    }, 1000)
+});
 
 // collects initials and time left, creates a score object, and saves it to local storage
-saveButton.addEventListener("click", function(event) {
-    event.preventDefault();
+saveBtn.addEventListener("click", function(event) {
+    event.preventDefault(); //suppress form behavior
 
-    console.log("save button clicked");
-
-    // make a high score object
+    // make a high score object to store data
     var newHighScore = {
-        initials : document.getElementById("initials-input").value,     // grab the initials
+        initials : document.getElementById("initials-input").value,    // grab the initials
         score : timeLeft
     };
+    document.getElementById("initials-input").value = "";              // clear input field
 
-    console.log(newHighScore);
-
-    // save new high score to array
+    // save new high score obj to array
     highScores.push(newHighScore);
 
     // save high score array to localStorage
     localStorage.setItem("codeQuizHighScores", JSON.stringify(highScores));
-})
+});
+
+// deletes high scores from local storage
+clearBtn.addEventListener("click", function (event) {
+    highScores = [];
+    localStorage.removeItem("codeQuizHighScores");
+    writeHighScores();
+});
+
+// displays high scores card
+scoresBtn.addEventListener("click", function (event) {
+    console.log("High Scores button clicked");
+});
+
+// back to start card
+backBtn.addEventListener("click", function () {
+    console.log("Back button clicked");
+});
+
+// need a function to write high scores to game box
+function writeHighScores () {
+    clearScoreTable();
+
+    const scoreRow = document.createElement("tr");  // create a <tr>
+
+    const initHead = document.createElement("th");  // create a <th>
+    initHead.textContent = "Initials";              // add "Initials" to <th>
+    scoreRow.append(initHead);                      // append to <tr>
+
+    const scoreHead = document.createElement("th"); // create a <th>
+    scoreHead.textContent = "Score";                // add "score" to <th>
+    scoreRow.append(scoreHead);                     // append to <tr>
+
+    scoreTable.append(scoreRow);                    // append to table
+
+    // loop over high scores array
+    // for each element in array
+        // 
+
+
+
+}
+
 
 
 // retrieves high scores from local storage
@@ -89,32 +161,7 @@ var localHighScores = JSON.parse(localStorage.getItem("codeQuizHighScores"));
     }
 }
 
-playButton.addEventListener("click", startGame);
 
-function startGame (event) {
-    timeLeft = startTime;
-    index = 0;
-    playButton.style.display = "none";
-    feedbackBox.style.display = "block";
-    feedbackBox.textContent = "";
-    gameHeading.textContent = "Good Luck!";
-
-    // start the timer
-    gameTimer = setInterval(function() {
-        timerBox.textContent = "Time Left: " + timeLeft;
-        timerBox.style.visibility = "visible";
-        if (timeLeft <= 0) {
-            timerBox.style.visibility = "hidden";
-            defeat();
-        }
-        timeLeft--;
-    }, 1000);
-
-    setTimeout(function() {
-        gameHeading.style.display = "none";
-        writeQuestion(index);
-    }, 1000)
-}
 
 // writes the contents of the question object at the given index
 function writeQuestion (index) {
@@ -124,9 +171,10 @@ function writeQuestion (index) {
     var answers = Object.values(questionArray[index]);
 
     // create a header for the question
-    var questionHeading = document.createElement("h2");
+    var questionHeading = document.createElement("h3");
     questionHeading.textContent = answers[0];
     gameBox.append(questionHeading);
+    
 
     // generate answers
     for (i = 1; i < answers.length; i++) {
@@ -137,7 +185,7 @@ function writeQuestion (index) {
 
         // add text and attach to document
         answerEl.textContent = answers[i];
-        answerEl.setAttribute("class", "game-button")
+        answerEl.setAttribute("class", "game-button");
         gameBox.append(answerEl);
     }
 }    
@@ -197,5 +245,11 @@ function defeat () {
 function clearGameBox () {
     while (gameBox.hasChildNodes()) {
         gameBox.removeChild(gameBox.firstChild);
+    }
+}
+
+function clearScoreTable () {
+    while (scoreTable.hasChildNodes()) {
+        scoreTable.removeChild(scoreTable.firstChild);
     }
 }
