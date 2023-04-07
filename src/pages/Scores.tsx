dc60f6d568676b87ac043b8bd78dc6dca20776ce
Robert Adams <adamsiii.robert@gmail.com>
2023-04-07 10:35:43 -0700

    update Scores page

--- src/pages/Scores.tsx
+++ src/pages/Scores.tsx
@@ -1,16 +1,7 @@
-import { Button } from "@chakra-ui/react";
-
-import { Page } from "../@types";
-
-interface IScoreProps {
-  setPage: React.Dispatch<React.SetStateAction<Page>>;
-}
-
-export const Scores: React.FC<IScoreProps> = ({ setPage }): JSX.Element => {
+export const Scores: React.FC = (): JSX.Element => {
   return (
     <>
       <h1>Scores</h1>
-      <Button onClick={() => setPage(Page.HOME)}>Back</Button>
     </>
   );
 };
