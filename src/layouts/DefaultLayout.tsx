c52681e21c6d86e1bbbc9112b22f7fc0dd60a672
Robert Adams <adamsiii.robert@gmail.com>
2023-04-07 10:21:18 -0700

    factor out Main component

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,7 +1,8 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Flex, SimpleGrid } from "@chakra-ui/react";
 import { Sidebar } from "../components/Sidebar";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import { CONFIG } from "../config";
 
 interface LayoutProps {
   children: string | JSX.Element | JSX.Element[]; // | (() => JSX.Element);
@@ -9,13 +10,25 @@ interface LayoutProps {
 
 export const DefaultLayout: React.FC<LayoutProps> = ({ children }): JSX.Element => {
   return (
-    <Box h="100vh" display="flex" flexDir="column">
+    <Flex h="100vh" flexDir="column">
       <Header />
-      <SimpleGrid columns={2} gridTemplateColumns={`${20}% ${80}%`} flexGrow={1}>
-        <Sidebar />
-        <Box>{children}</Box>
-      </SimpleGrid>
+      {CONFIG.USE_SIDEBAR ? (
+        <SimpleGrid columns={2} gridTemplateColumns={`${20}% ${80}%`} flexGrow={1}>
+          <Sidebar />
+          <Main children={children} />
+        </SimpleGrid>
+      ) : (
+        <Main children={children} />
+      )}
       <Footer />
-    </Box>
+    </Flex>
+  );
+};
+
+const Main: React.FC<LayoutProps> = ({ children }): JSX.Element => {
+  return (
+    <Flex flexGrow={1} borderRadius="3xl" mx={5} bg="whiteAlpha.100">
+      {children}
+    </Flex>
   );
 };
