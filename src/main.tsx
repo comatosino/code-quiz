5de5c6a9b0dd9d07fa057053bc840fe8b21578b2
Robert Adams <adamsiii.robert@gmail.com>
2023-04-07 10:30:24 -0700

    move ColorModeToggle

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { theme } from "./theme";
 import { App } from "./App";
-import { ColorModeToggle } from "./components/ColorModeToggle";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <ColorModeToggle />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
