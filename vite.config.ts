86c6d6840e49ad4be43100956069e10535f682d9
Robert Adams <adamsiii.robert@gmail.com>
2023-08-14 20:59:27 -0700

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+import project from "./package.json";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${project.name}/`,
 });
