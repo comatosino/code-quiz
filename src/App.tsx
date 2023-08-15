a344596b7b4166b5e316f59091b98d8d7e6aaae9
Robert Adams <adamsiii.robert@gmail.com>
2023-08-14 22:49:04 -0700

    rename router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Provider } from "react-redux";
 
 import { store } from "./store";
-import { AppRouter } from "./AppRoutes";
+import { AppRouter } from "./AppRouter";
 
 export const App: React.FC = (): JSX.Element => {
   return (
