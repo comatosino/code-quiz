295a93b46bcdca2cef6103ce38d13d4983a93398
Robert Adams <adamsiii.robert@gmail.com>
2023-08-14 21:02:45 -0700

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import project from "./package.json";
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${project.name}/`,
+  base: "/code-quiz/",
 });
