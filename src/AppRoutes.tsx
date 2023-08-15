e486d0c01dca34d2f34343c9d02a1104813c12ce
Robert Adams <adamsiii.robert@gmail.com>
2023-08-14 21:24:31 -0700

    update Route pathing

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,18 +3,26 @@ import { Routes, Route } from "react-router-dom";
 import { Home, Legacy, Settings, _404 } from "./routes";
 import { DefaultLayout } from "./layouts";
 
+const BASE = "quizality";
+
+const path = (pathname: string) => `${BASE}/${pathname}`;
+
 export const AppRouter: React.FC = (): JSX.Element => {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
+      <Route path={BASE} element={<DefaultLayout />}>
         <Route index element={<Home />} />
-        <Route path="legacy" element={<Legacy />}>
+
+        {/* legacy quiz game */}
+        <Route path={path("legacy")} element={<Legacy />}>
           <Route index element={<Legacy.Home />} />
-          <Route path="quiz" element={<Legacy.Quiz />} />
-          <Route path="gameover" element={<Legacy.GameOver />} />
-          <Route path="scores" element={<Legacy.Scores />} />
+          <Route path={path("quiz")} element={<Legacy.Quiz />} />
+          <Route path={path("gameover")} element={<Legacy.GameOver />} />
+          <Route path={path("scores")} element={<Legacy.Scores />} />
         </Route>
-        <Route path="settings" element={<Settings />} />
+
+        <Route path={path("settings")} element={<Settings />} />
+
         <Route path="*" element={<_404 />} />
       </Route>
     </Routes>
