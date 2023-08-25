900b175831ab311384ee5bdecb17537f1030e05f
Robert Adams <adamsiii.robert@gmail.com>
2023-08-24 20:06:08 -0700

    mv toggle to right-side

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { ColorModeToggle } from "../ColorModeToggle";
 export const Header: React.FC = (): JSX.Element => {
   return (
     <header>
-      <Flex w="full" h="10vh" px={5} alignItems="center">
+      <Flex w="full" h="10vh" px={5} alignItems="center" justifyContent="end">
         <ColorModeToggle size="lg" />
       </Flex>
     </header>
