a865eb95d8978f8956e487a8881b7bca1784e21e
Robert Adams <adamsiii.robert@gmail.com>
2023-08-24 20:05:48 -0700

    update paths, add quiz routes

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Home, Legacy, _404 } from "./routes";
+import { Home, Quiz, Legacy, _404 } from "./routes";
 import { DefaultLayout } from "./layouts";
 
 export const AppRouter: React.FC = (): JSX.Element => {
@@ -11,14 +11,16 @@ export const AppRouter: React.FC = (): JSX.Element => {
         <Route index element={<Home />} />
 
         {/* quiz */}
-        <Route path="quiz">
-          <Route index></Route>
+        <Route path="quiz" element={<Quiz />}>
+          <Route index element={<Quiz.New />} />
+          <Route path="play" element={<Quiz.Play />} />
+          <Route path="gameover" element={<Quiz.GameOver />} />
         </Route>
 
         {/* legacy quiz */}
-        <Route path="legacy" element={<Legacy />}>
-          <Route index element={<Legacy.Home />} />
-          <Route path="quiz" element={<Legacy.Quiz />} />
+        <Route path="quiz-legacy" element={<Legacy />}>
+          <Route index element={<Legacy.New />} />
+          <Route path="play" element={<Legacy.Play />} />
           <Route path="gameover" element={<Legacy.GameOver />} />
           <Route path="scores" element={<Legacy.Scores />} />
         </Route>
