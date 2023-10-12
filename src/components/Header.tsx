083ceae51ea3eea963345f71fc31c3bf5511783b
Robert Adams <r0b4dams@gmail.com>
2023-10-12 09:15:12 -0700

    add logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, Text } from '@chakra-ui/react';
+import { Box, Center, Container, Flex, HStack, Image, Text } from '@chakra-ui/react';
 import { ColorModeToggle } from './ColorModeToggle';
 
 export const Header: React.FC = (): JSX.Element => {
@@ -6,12 +6,17 @@ export const Header: React.FC = (): JSX.Element => {
     <Box as='header' bgColor='brand.500'>
       <Container maxW='container.xl'>
         <Flex w='full' h='10vh' px={5} alignItems='center' justifyContent='space-between'>
-          <Box>
-            <Text fontSize='4xl'>Quizality</Text>
-            <Text ml={10} fontSize='xl'>
-              a trivia game
-            </Text>
-          </Box>
+          <HStack>
+            <Center w={24} h={24} borderRadius='full' bgColor='#ddd'>
+              <Image mr={1} w='90%' src={`${import.meta.env.BASE_URL}/logo.svg`} />
+            </Center>
+            <Box>
+              <Text fontSize='4xl'>Quizality</Text>
+              <Text ml={10} fontSize='xl'>
+                a trivia game
+              </Text>
+            </Box>
+          </HStack>
           <ColorModeToggle size='lg' />
         </Flex>
       </Container>
