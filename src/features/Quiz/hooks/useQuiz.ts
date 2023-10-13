bd3d8f7268225c0a9aab89ccb75c2ad9a2813bbb
Robert Adams <r0b4dams@gmail.com>
2023-10-12 20:35:08 -0700

    refactor quiz questions to avoid reshuffle

--- src/features/Quiz/hooks/useQuiz.ts
+++ src/features/Quiz/hooks/useQuiz.ts
@@ -7,66 +7,75 @@ import { correct, incorrect } from '../helpers/feedback';
 import { decodeHtml, shuffle } from '../../../utils';
 
 export const useQuiz = () => {
-  const dispatch = useAppDispatch();
   const quiz = useAppSelector((state) => state.quiz);
-  const { data, error, isFetching } = useGetQuestionsQuery(quiz.params, {
-    refetchOnMountOrArgChange: true,
-  });
+  const dispatch = useAppDispatch();
 
   const score = useRef(0);
   const [index, setIndex] = useState(0);
+  const [questions, set] = useState<IQuestion[]>([]);
   const [inProgress, setInProgress] = useState(false);
   const [gameover, setGameover] = useState(false);
   const toast = useToast();
 
+  const { data, error, isFetching } = useGetQuestionsQuery(quiz.params, {
+    refetchOnMountOrArgChange: true,
+  });
+
   useEffect(() => {
-    if (inProgress && data && index >= data.results.length) {
-      const result = {
-        numCorrect: score.current,
-        numQuestions: data?.results.length || 0,
-        percentage: score.current / data.results.length,
-      };
-      dispatch(setLastScore(result));
-      setGameover(true);
+    if (data && data.results.length) {
+      const formatted: IQuestion[] = data.results.map((item) => {
+        const text = decodeHtml(item.question);
+        const correct = decodeHtml(item.correct_answer);
+        const choices = item.incorrect_answers.map((a) => decodeHtml(a));
+        choices.push(correct);
+        shuffle(choices);
+        return { text, correct, choices };
+      });
+      set(formatted);
     }
-  }, [index]);
+  }, [isFetching]);
 
-  let currQuestion, correctAnswer: string;
-  const otbdQuestion = data?.results[index];
-
-  if (otbdQuestion) {
-    const question = decodeHtml(otbdQuestion.question);
-
-    // correct and incorrect answers arrive html encoded on different props
-    // add all decoded strings to a new array and shuffle it
-    correctAnswer = decodeHtml(otbdQuestion.correct_answer);
-    const allAnswers = otbdQuestion.incorrect_answers.map((text) => decodeHtml(text));
-    allAnswers.push(correctAnswer);
-    const choices = shuffle(allAnswers);
-
-    currQuestion = {
-      question,
-      choices,
-      category: otbdQuestion.category,
-      difficulty: otbdQuestion.difficulty,
-    } as IQuizQuestion;
-  }
+  const no_data = questions.length === 0;
+  const question = questions[index];
 
   const startQuiz = () => {
     setInProgress(true);
   };
 
-  const checkAnswer = (choice: string) => {
-    if (choice === correctAnswer) {
+  const nextQuestion = () => {
+    if (index + 1 >= questions.length) {
+      setGameover(true);
+      const result = {
+        numCorrect: score.current,
+        numQuestions: data?.results.length || 0,
+        percentage: score.current / questions.length,
+      };
+      dispatch(setLastScore(result));
+    } else {
+      setIndex((prev) => prev + 1);
+    }
+  };
+
+  const checkAnswer = (choice: string | null) => {
+    if (choice === questions[index].correct) {
       score.current++;
       toast(correct);
     } else {
       toast(incorrect);
     }
-    setIndex((prev) => prev + 1);
+    nextQuestion();
   };
 
-  const noQs = data?.results.length === 0;
-
-  return { error, isFetching, inProgress, startQuiz, currQuestion, checkAnswer, gameover, noQs };
+  return {
+    error,
+    isFetching,
+    inProgress,
+    gameover,
+    index,
+    question,
+    no_data,
+    startQuiz,
+    nextQuestion,
+    checkAnswer,
+  };
 };
