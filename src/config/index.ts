65f818995f5a4bd399ce9fdf787aff2ae79b6332
Robert Adams <r0b4dams@gmail.com>
2023-10-16 13:11:52 -0700

    update with vite meta props

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export const CONFIG = {
+  BASE_URL: import.meta.env.BASE_URL,
   ENABLE_LEGACY: import.meta.env.VITE_ENABLE_LEGACY === 'true',
-  LOCAL_STORAGE_KEY_LEGACY: 'legacy-quiz-high-scores',
+  LOCAL_STORAGE_KEY_LEGACY: import.meta.env.VITE_LOCAL_STORAGE_KEY,
 };
